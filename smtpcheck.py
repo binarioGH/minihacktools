9f3ceaceb29a8ce706a85414d9219b56d2eb40a0
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-06-25 16:37:19 -0500

    smtpcheck

---
+++ smtpcheck.py
@@ -0,0 +1,43 @@
+#!/urs/bin/python
+#-*-coding: utf-8-*-
+from socket import *
+from sys import argv
+
+
+
+def h():
+	print("\nDebes usar esta sintaxis:\n{} (objetivo) (comando) (archivo con usuarios)".format(argv[0]))
+	exit()
+
+
+if __name__ == '__main__':
+	try:
+		if argv[1] == "-h":
+			h()
+		target = argv[1]
+		command = argv[2]
+		file = argv[3]
+
+	except:
+		print("{} -h para ver las opciones".format(argv[0]))
+		exit()
+
+	sock = socket(AF_INET, SOCK_STREAM)
+	try:
+		sock.connect((target,25))
+		banner = sock.recv(1024)
+		print(banner)
+		if "220" in banner:
+			with open(file, "r") as f:
+				for user in f:
+					sock.send("{} {}".format(command, user))
+					result = sock.recv(1024)
+					if "252" in str(result) or "250" in str(result):
+						print("Usuario valido: {}".format(user))
+		sock.close()
+	except:
+		print("No se pudo conectar a: {}".format(target))
+
+
+
+
