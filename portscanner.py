f62a3df0bce5bdc8882429a308b3245d3a6b06fc
binarioGH <lacuentafalsadediegojonas@gmail.com>
2018-07-14 12:04:27 -0500

    Pequeñas mejoras a el scanner de puertos y un archivo con banners vulnerables (sacado de aquí: https://github.com/bigsnarfdude/violentPythonForHackers/blob/master/CH1/vuln-banners.txt (pero le agregué 3 banners))

--- portscanner.py
+++ portscanner.py
@@ -14,17 +14,57 @@ def banner():
 		|    |         
 		|    |         
 		|____|        
-		''')             
+		''')
+def bannerscann(banners, bf):
+	try:
+		vulbanners = open(bf, "r")
+	except:
+		print("No se ha podido abrir {}".format(b))
+		exit()
+	else:
+		print("Se ha empezado el analisis de banners.\n\n")
+		for banner in banners:
+			banner += "\n"
+			print("[+] Analizando: {}".format(banner))
+			count = 0
+			for b in vulbanners:
+				count += 1
+				if b in banner or b == banner:
+					print("Se ha encontrado un banner vulnerable: {}".format(b))
+					break
+		print("Se ha terminado el analisis de banners.")
+
+
+def portscann(minim, maxim):
+	bnners = []
+	print("\nSe ha empezado el analisis de puertos...\n")
+	
+	for port in range(minim, maxim + 1):
+		server = socket(AF_INET, SOCK_STREAM)
+		server.settimeout(2)
+		if(server.connect_ex((ip, port)) == 0):
+			print("\n\nPuerto abierto: {}.".format(port))
+			try:
+				banner = server.recv(1024).decode()
+				print("Banner: {}".format(banner))
+				bnners.append(banner)
+			except:
+				continue
+			server.close()
+	print("\n\nAnalisis de puertos terminado.")
+	return bnners
 
 def h():
+	print("Usos: {}: [-min num] [-max num] [-i ip] [-vb archivo]\n\n".format(argv[0]))
 	print("Opciones:\n\n-min: Establecer el puerto por el que se empezará a escanear.")
 	print("-max: Establecer el ultimo puerto que se escaneará.")
 	print("-i: Establecer que la ip del host que se va a escanear.")
+	print("-vb: Analizar los banners vulnerables (Tienes que agregar un archivo con banners vulnerables).")
 	exit()
 
 if __name__ == '__main__':
 	banner()
-	if len(argv) != 7 and len(argv) != 2:
+	if len(argv) != 7 and len(argv) != 2 and len(argv) != 9:
 		print("Usa {} -h para ver las opciones.".format(argv[0]))
 		exit()
 	else:
@@ -32,6 +72,8 @@ if __name__ == '__main__':
 		minim = 0
 		maxim = 0
 		argcount = 0
+		vb = ""
+		vbe = False
 		for arg in argv:
 			if arg[0] != "-":
 				argcount += 1
@@ -44,19 +86,19 @@ if __name__ == '__main__':
 				minim = int(argv[argcount + 1])
 			elif arg == "-max":
 				maxim = int(argv[argcount + 1])
+			elif arg == "-vb":
+				vb = argv[argcount + 1]
+				vbe = True
 			else:
 				print("Usa {} -h para ver las opciones.".format(argv[0]))
 				exit()
 			argcount += 1
-		print("\nSe ha empezado el analisis de puertos...\n")
-		for port in range(minim, maxim + 1):
-			server = socket(AF_INET, SOCK_STREAM)
-			server.settimeout(2)
-			if(server.connect_ex((ip, port)) == 0):
-				print("\n\nPuerto abierto: {}.".format(port))
-			try:
-				print("Banner: {}".format(server.recv(1024).decode()))
-			except:
-				continue
-			server.close()
-		print("\n\nAnalisis terminado.")
+
+		print("{} {} {}".format(minim, maxim, ip))
+		bnners = portscann(minim, maxim)
+		if vbe == True:
+			bannerscann(bnners, vb)
+		else:
+			exit()
+
+		
