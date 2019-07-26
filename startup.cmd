3a4062f3afa629324701ea5cd8376b9c4a070dd7
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-07-25 23:58:31 -0700

    start up a program with windows

--- startup.cmd
+++ startup.cmd
@@ -1,13 +1,14 @@
 @echo off
-set dest='%appdata%\Microsoft\Windows\Start Menu\Programs\Startup'
+set dest=%appdata%\Microsoft\Windows\Start Menu\Programs\Startup
+cd %dest%	
 echo dir: %dest%
 :movestuff
 if %2 == -l goto link
+echo Copying!
 copy %1 %dest%
 goto ext
-
 :link
-echo start %1 >> link.bat
-move link.bat %dest%
+echo Linking!
+echo start %1 > link.bat
 :ext
 echo bye
