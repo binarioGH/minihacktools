22b69ae9a53677aac15576dfdd9bf3e42ff6a2c3
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-08-23 20:31:41 -0700

    miadooooos

--- miados.py
+++ miados.py
@@ -1,7 +1,7 @@
 #-*-coding: utf-8-*-
 from time import strftime
 from subprocess import run
-
+from random import randint
 def startLink(link):
 	run("start {}".format(link), shell=True)
 
@@ -9,9 +9,17 @@ def startLink(link):
 def main():
 	date = strftime("%d/%m/%y")
 	print(date)
-	if date == "21/09/19":
+	if date == "23/08/19":
 		print("El día de los miados ha llegado")
-		startLink("https://www.pornhub.com/view_video.php?viewkey=ph5925e3ff33db1")
+		links_miados = (
+			"https://www.youtube.com/watch?v=KCWEedgFNCk",
+			"https://www.youtube.com/watch?v=Ky22ke22IVA",
+			"https://www.youtube.com/watch?v=oKK3xwRDAys",
+			"https://www.youtube.com/watch?v=w8dqPJYIA2U",
+			"https://www.youtube.com/watch?v=XR1GF-AgSPQ"
+			)
+		link_seleccionado = links_miados[randint(0, len(links_miados) - 1)]
+		startLink(link_seleccionado)
 
 if __name__ == '__main__':
 	main()
