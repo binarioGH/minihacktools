494c13d27a9e972f2fa37cc2da82bd95713ba100
Diego Jonás Pérez <35743432+binarioGH@users.noreply.github.com>
2021-03-03 23:35:15 -0800

    still working on automating browser

--- automaticanswers.js
+++ automaticanswers.js
@@ -13,20 +13,31 @@ let next = document.getElementsByClassName("FrameRight")[0];
 video.addEventListener("ended", function(){next.click()});
 
 
-let completed = document.getElementsByClassName("done-complete");
+let important_butts = document.getElementsByClassName("answer-choice-button");
 let first = document.getElementsByClassName("done-start");
 let retry = document.getElementsByClassName("done-retry");
 let total = 0;
 
 setInterval(function(){ 
-	completed = document.getElementsByClassName("done-complete");
+	let next = document.getElementsByClassName("FrameRight")[0];
+	important_butts = document.getElementsByClassName("answer-choice-button");
+	let important = Array.prototype.slice.call(important_butts, 0);
 	first = document.getElementsByClassName("done-start");
 	retry = document.getElementsByClassName("done-retry");
 	let a1 = Array.prototype.slice.call(first, 0);
 	let a2 = Array.prototype.slice.call(retry, 0);
-	let total_questions = a1.concat(a2);
+	let answers = a1.concat(a2);
+	let total_questions = important.concat(answers);
 	for(question of total_questions){
 		question.click();
 	}
 	next.click();
 }, 500);
+
+
+/*
+
+
+Assessment_Main_Body_Content_Question - test questions
+
+*/
